1460b0457621ebcc8edc260afa3a0fefdf9a7468
JustinKarso <justinkarsopawiro@gmail.com>
2023-03-04 00:40:44 +0100

    fix: github link

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ const Main = ({ styles }) => (
     </ScrollWrapper>
     <div className="flex flex-row gap-4 mt-4 justify-center">
       <Link
-        href="https://github.com/justinkarso/scrollbar"
+        href="https://github.com/justinkarso/scrollchain"
         target="_blank"
         className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 hover:-translate-y-[2px] active:translate-y-[2px] transition-all flex items-center justify-center"
       >
